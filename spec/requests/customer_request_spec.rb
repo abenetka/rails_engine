68b1dcb4695a4281a5d750409ac4eb818a7a010a
abenetka <ali.benetka@gmail.com>
2019-01-23 15:42:44 -0700

    Adds test to find a merchant by id

--- spec/requests/customer_request_spec.rb
+++ spec/requests/customer_request_spec.rb
@@ -1,15 +1,37 @@
 require 'rails_helper'
 
 describe "Customers API" do
-  it "sends a list of customers" do
+  it "shows a list of customers" do
     create_list(:customer, 3)
 
     get '/api/v1/customers'
 
     expect(response).to be_successful
 
-    customers = JSON.parse(response.body)["data"]
+    customers = JSON.parse(response.body)
 
-    expect(customers.count).to eq(3)
+    expect(customers["data"].count).to eq(3)
   end
+
+  it "can show a customer" do
+    customer_id = create(:customer).id
+
+    get "/api/v1/customers/#{customer_id}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["data"]["id"]).to eq(customer_id.to_s)
+  end
+
+  it "can find a customer by id" do
+    customer_id = create(:customer).id
+
+    get "/api/v1/customers/find?id=#{customer_id}"
+
+    customer = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(customer["data"]["id"]).to eq(customer_id.to_s)
+  end
+
 end
