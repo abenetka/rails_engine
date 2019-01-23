4092ff4cfe636fc59589907be533f0a427618fbd
abenetka <ali.benetka@gmail.com>
2019-01-23 16:22:16 -0700

    Adds routes for merchants and search controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
       namespace :customers do
         get '/find', to: 'search#show'
       end
-      
+
+      namespace :merchants do
+        get '/find', to: 'search#show'
+      end
+
       resources :customers, only: [:index, :show]
 
       resources :items, only: [:index]
-      resources :merchants, only: [:index]
+      resources :merchants, only: [:index, :show]
     end
   end
 end
