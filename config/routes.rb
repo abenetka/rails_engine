7e21bda155111319385a61e2cf346d23115a1423
abenetka <ali.benetka@gmail.com>
2019-01-23 15:44:23 -0700

    Adds routes to find customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index]
-      resources :customers, only: [:index]
+      namespace :customers do
+        get '/find', to: 'search#show'
+      end
+      
+      resources :customers, only: [:index, :show]
+
       resources :items, only: [:index]
+      resources :merchants, only: [:index]
     end
   end
 end
