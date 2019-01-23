fe8ef18e5f2ec2076c5353eb31ef24b39ea3c2d0
abenetka <ali.benetka@gmail.com>
2019-01-23 15:59:52 -0700

    Adds tests to search by first name and last name

--- spec/requests/customer_request_spec.rb
+++ spec/requests/customer_request_spec.rb
@@ -34,4 +34,25 @@ describe "Customers API" do
     expect(customer["data"]["id"]).to eq(customer_id.to_s)
   end
 
+  it "can find a customer by first name" do
+    customer_name = create(:customer).first_name
+
+    get "/api/v1/customers/find?first_name=#{customer_name}"
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["data"]["attributes"]["first_name"]).to eq(customer_name.to_s)
+  end
+
+  it "can find a customer by last name" do
+    customer_last = create(:customer).last_name
+
+    get "/api/v1/customers/find?last_name=#{customer_last}"
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["data"]["attributes"]["last_name"]).to eq(customer_last.to_s)
+  end
+
+
 end
