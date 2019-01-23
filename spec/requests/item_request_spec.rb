95dbc2d8cc6454a1dff5941c85b793db7ffc07f4
abenetka <ali.benetka@gmail.com>
2019-01-23 14:32:11 -0700

    Updates item index endpoint test

--- spec/requests/item_request_spec.rb
+++ spec/requests/item_request_spec.rb
@@ -4,9 +4,11 @@ describe "Items API" do
   it "sends a list of items" do
     create_list(:item, 3)
 
-    get '/api/v1/items.json'
+    get '/api/v1/items'
 
-    items = JSON.parse(response.body)
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)["data"]
 
     expect(items.count).to eq(3)
   end
