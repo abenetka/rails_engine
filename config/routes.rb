ece19f023bbb6f15745dd654fdefb81ad902d97d
abenetka <ali.benetka@gmail.com>
2019-01-24 14:41:27 -0700

    Adds routes for new finds and remaining required controllers fromcsv data

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,28 @@ Rails.application.routes.draw do
         get '/most_revenue', to: 'most_revenue#index'
       end
 
-      resources :customers, only: [:index, :show]
+      namespace :transactions do
+        get '/find', to: 'search#show'
+      end
 
-      resources :items, only: [:index]
+      namespace :invoice_items do
+        get '/find', to: 'search#show'
+      end
+
+      namespace :invoices do
+        get '/find', to: 'search#show'
+      end
+
+      namespace :items do
+        get '/find', to: 'search#show'
+      end
+
+      resources :customers, only: [:index, :show]
+      resources :items, only: [:index, :show]
       resources :merchants, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
+      resources :invoice_items, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
     end
   end
 end
