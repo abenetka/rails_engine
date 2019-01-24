18c0edc9e055e813adccdd552e1709c312a4f293
abenetka <ali.benetka@gmail.com>
2019-01-24 14:39:56 -0700

    Updates most revenue method for more AR than sql

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,12 +4,12 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   def self.most_revenue(quantity)
-    Merchant.joins(
+    joins(
       'inner join invoices i on i.merchant_id=merchants.id
        inner join invoice_items ii on ii.invoice_id=i.id
        inner join transactions t on t.invoice_id=i.id')
       .select('merchants.*, sum(ii.quantity * ii.unit_price) as revenue')
-      .where('t.result=?', 'success')
+      .where(t: {result: 0})
       .order('revenue desc')
       .group(:id)
       .limit(quantity)
