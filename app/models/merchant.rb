b4dc7d729bd25e0d33248983bace3c8978f67b9c
abenetka <ali.benetka@gmail.com>
2019-01-23 21:54:45 -0700

    Adds most_revenue method to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,17 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
   has_many :items
+  has_many :invoices
+
+  def self.most_revenue(quantity)
+    Merchant.joins(
+      'inner join invoices i on i.merchant_id=merchants.id
+       inner join invoice_items ii on ii.invoice_id=i.id
+       inner join transactions t on t.invoice_id=i.id')
+      .select('merchants.*, sum(ii.quantity * ii.unit_price) as revenue')
+      .where('t.result=?', 'success')
+      .order('revenue desc')
+      .group(:id)
+      .limit(quantity)
+  end
 end
