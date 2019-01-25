8ed2ef7c830a8c42e28b2cfd136ad18a84e0c319
abenetka <ali.benetka@gmail.com>
2019-01-24 21:46:50 -0700

    adds index for find all invoice items

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::InvoiceItems::SearchController < ApplicationController
 
+  def index
+    render json: InvoiceItemSerializer.new(InvoiceItem.find_all(invoice_items_params))
+  end
+  
   def show
     render json: InvoiceItemSerializer.new(InvoiceItem.find_by(invoice_items_params))
   end
