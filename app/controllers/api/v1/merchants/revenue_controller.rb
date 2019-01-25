87e0d5155b4774063bb1778e8a6e5fea9c3e9b33
abenetka <ali.benetka@gmail.com>
2019-01-25 16:49:42 -0700

    Adds show to revenue controller and updates index

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def index
-    render json: MerchantSerializer.new(Merchant.total_revenue_by_date(params[:date]))
+    render json: { data: {revenue_by_date: {all_merchants: Merchant.total_revenue_by_date(params[:date])}}}
+  end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: { data: {revenue_for_merchant: {merchant: merchant.total_revenue_for_merchant}}}
   end
 
 end
