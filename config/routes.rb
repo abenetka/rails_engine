2bc33ff6af72e3b5647b82fd29d5c5ceae90cb6a
abenetka <ali.benetka@gmail.com>
2019-01-24 21:45:21 -0700

    Adds routes for find alls and msot revenue,revenue, most items

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find', to: 'search#show'
         get '/most_revenue', to: 'most_revenue#index'
+        get '/most_items', to: 'most_items#index'
+        get '/revenue', to: 'revenue#index'
       end
 
       namespace :transactions do
@@ -16,10 +18,12 @@ Rails.application.routes.draw do
 
       namespace :invoice_items do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
 
       namespace :invoices do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
       end
 
       namespace :items do
