78d7084ad233be1a5e9ab7d3a2b39715fed8c206
abenetka <ali.benetka@gmail.com>
2019-01-24 21:42:15 -0700

    Adds tests for total items solds and total revenue for merchants by date

--- spec/requests/merchant_request_spec.rb
+++ spec/requests/merchant_request_spec.rb
@@ -71,26 +71,26 @@ end
 
 describe "Merchant stats" do
   before :each do
-    @merchant_1 = create(:merchant)
-    @merchant_2 = create(:merchant)
-    @merchant_3 = create(:merchant)
+    @merchant_1 = create(:merchant, name: "Merchant 1")
+    @merchant_2 = create(:merchant, name: "Merchant 2")
+    @merchant_3 = create(:merchant, name: "Merchant 3")
 
     @item_1 = create(:item, merchant: @merchant_1)
     @item_2 = create(:item, merchant: @merchant_2)
     @item_3 = create(:item, merchant: @merchant_3)
 
     @invoice_1 = create(:invoice, merchant: @merchant_1)
-    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 1, unit_price: 100, created_at: 10.minutes.ago, updated_at: 9.minute.ago)
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 1, unit_price: 2000)
 
     @invoice_2 = create(:invoice, merchant: @merchant_2)
-    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 3, unit_price: 300, created_at: 2.days.ago, updated_at: 1.minute.ago)
+    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 3, unit_price: 300)
 
     @invoice_3 = create(:invoice, merchant: @merchant_3)
-    @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_3, quantity: 3, unit_price: 200, created_at: 10.minutes.ago, updated_at: 5.minute.ago)
+    @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_3, quantity: 5, unit_price: 200)
 
-    @transaction_1 = create(:transaction, invoice: @invoice_1, result: 'success')
-    @transaction_2 = create(:transaction, invoice: @invoice_2, result: 'success')
-    @transaction_3 = create(:transaction, invoice: @invoice_3, result: 'success')
+    @transaction_1 = create(:transaction, invoice: @invoice_1, result: 'success', updated_at: '2012-03-27 14:54:09 UTC')
+    @transaction_2 = create(:transaction, invoice: @invoice_2, result: 'success', updated_at: '2012-03-27 14:54:09 UTC')
+    @transaction_3 = create(:transaction, invoice: @invoice_3, result: 'success', updated_at: '2012-03-26 14:54:09 UTC')
   end
 
 
@@ -98,8 +98,30 @@ describe "Merchant stats" do
     quantity = 5
     get "/api/v1/merchants/most_revenue?quantity=#{quantity}"
 
+    merchants = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchants["data"].first["attributes"]["name"]).to eq("Merchant 1")
+    expect(merchants["data"].last["attributes"]["name"]).to eq("Merchant 2")
+  end
+
+  it "returns the top x merchants ranked by total items sold" do
+    quantity = 5
+    get "/api/v1/merchants/most_items?quantity=#{quantity}"
+
     merchant = JSON.parse(response.body)
 
     expect(response).to be_successful
   end
+
+  it "returns the top revenue for merchants by date x" do
+    date = "2012-03-27 14:54:09 UTC"
+
+    get "/api/v1/merchants/revenue?date=#{date}"
+    binding.pry
+    merchant = JSON.parse(response.body)
+    expect(response).to be_successful
+  end
+
+
 end
