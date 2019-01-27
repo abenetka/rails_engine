51d05180cd03e0d61f39885625d8bcf4e07b9a79
abenetka <ali.benetka@gmail.com>
2019-01-27 15:09:35 -0700

    Adds index

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Merchants::SearchController < ApplicationController
 
+  def index
+    render json: MerchantSerializer.new(Merchant.where(merchant_params))
+  end
+
   def show
     render json: MerchantSerializer.new(Merchant.find_by(merchant_params))
   end
