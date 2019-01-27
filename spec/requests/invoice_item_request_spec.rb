70cc35b11a9fefd56d243a80e880901c80c691ff
abenetka <ali.benetka@gmail.com>
2019-01-27 13:39:37 -0700

    Adds tests for invoice item endpoints

--- spec/requests/invoice_item_request_spec.rb
+++ spec/requests/invoice_item_request_spec.rb
@@ -76,3 +76,50 @@ describe "Invoice Items API" do
     expect(returned_invoice_item["data"]["id"]).to eq(invoice_item.id.to_s)
   end
 end
+
+describe "Invoice Item relationship endpoints" do
+  before :each do
+    @merchant_1 = create(:merchant, name: "Merchant 1")
+    @customer_1 = create(:customer)
+    @customer_2 = create(:customer)
+
+    @item_1 = create(:item, merchant: @merchant_1)
+    @item_2 = create(:item, merchant: @merchant_1)
+    @item_3 = create(:item, merchant: @merchant_1)
+
+    @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_1, updated_at: '2012-03-27 14:54:09 UTC')
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 1, unit_price: 2000)
+    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_1, quantity: 3, unit_price: 300)
+
+    @invoice_2 = create(:invoice, merchant: @merchant_1, customer: @customer_2, updated_at: '2012-03-24 14:54:09 UTC')
+    @invoice_item_3 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 3, unit_price: 300)
+
+    @transaction_1 = create(:transaction, invoice: @invoice_1, result: 'success', updated_at: '2012-03-27 14:54:09 UTC')
+    @transaction_2 = create(:transaction, invoice: @invoice_1, result: 'failed', updated_at: '2012-03-27 14:54:09 UTC')
+    @transaction_3 = create(:transaction, invoice: @invoice_2, result: 'failed', updated_at: '2012-03-26 14:54:09 UTC')
+  end
+
+  it 'returns the associated invoice' do
+    invoice_id = @invoice_1.id
+    invoice_item_id = @invoice_item_1.id
+
+    get "/api/v1/invoice_items/#{invoice_item_id}/invoice"
+    returned = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(returned["data"]["type"]).to eq("invoice")
+    expect(returned["data"]["id"]).to eq(invoice_id.to_s)
+  end
+
+  it 'returns the associated item' do
+    invoice_item_id = @invoice_item_1.id
+    item_id = @item_1.id
+
+    get "/api/v1/invoice_items/#{invoice_item_id}/item"
+    returned = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(returned["data"]["type"]).to eq("item")
+    expect(returned["data"]["id"]).to eq(item_id.to_s)
+  end
+end
