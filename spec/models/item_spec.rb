bf60f55a49738e36c6c4f5a49f7198d2760de55c
abenetka <ali.benetka@gmail.com>
2019-01-27 10:23:54 -0700

    Adds tests for best day

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,54 @@ RSpec.describe Item, type: :model do
     it {should validate_presence_of(:description)}
     it {should validate_presence_of(:unit_price)}
   end
+
+  describe 'items stats' do
+    before :each do
+      @merchant_1 = create(:merchant)
+      @merchant_2 = create(:merchant)
+      @merchant_3 = create(:merchant)
+
+      @customer_1 = create(:customer)
+      @customer_2 = create(:customer)
+      @customer_3 = create(:customer)
+
+      @item_1 = create(:item, merchant: @merchant_1)
+      @item_2 = create(:item, merchant: @merchant_2)
+      @item_3 = create(:item, merchant: @merchant_3)
+
+      @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_1, updated_at: '2012-03-23 14:54:09 UTC')
+      @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 1, unit_price: 2000)
+
+      @invoice_2 = create(:invoice, merchant: @merchant_2, customer: @customer_1,  updated_at: '2012-03-24 14:54:09 UTC')
+      @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 3, unit_price: 300)
+
+      @invoice_3 = create(:invoice, merchant: @merchant_3, customer: @customer_2,  updated_at: '2012-03-23 14:54:09 UTC')
+      @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_3, quantity: 5, unit_price: 200)
+
+      @invoice_4 = create(:invoice, merchant: @merchant_1, customer: @customer_2,  updated_at: '2012-03-24 14:54:09 UTC')
+      @invoice_item_4 = create(:invoice_item, item: @item_1, invoice: @invoice_4, quantity: 1, unit_price: 200)
+
+      @transaction_1 = create(:transaction, invoice: @invoice_1, result: 'success', updated_at: '2012-03-27 14:54:09 UTC')
+      @transaction_2 = create(:transaction, invoice: @invoice_2, result: 'success', updated_at: '2012-03-27 14:54:09 UTC')
+      @transaction_3 = create(:transaction, invoice: @invoice_3, result: 'success', updated_at: '2012-03-26 14:54:09 UTC')
+    end
+
+    it 'calculates total revenue for x items' do
+      expect(Item.items_most_revenue(3)[0]).to eq(@item_1)
+      expect(Item.items_most_revenue(3)[1]).to eq(@item_3)
+      expect(Item.items_most_revenue(3)[2]).to eq(@item_2)
+    end
+
+    it 'calculates total items sold for x items' do
+      # GET /api/v1/items/most_items?quantity=x returns the top x item instances ranked by total number sold
+      expect(Item.top_items(3)[0]).to eq(@item_3)
+      expect(Item.top_items(3)[1]).to eq(@item_2)
+      expect(Item.top_items(3)[2]).to eq(@item_1)
+    end
+
+    it 'calculates best date for sales of single item ' do
+      # GET /api/v1/items/:id/best_day returns the date with the most sales for the given item using the invoice date. If there are multiple days with equal number of sales, return the most recent day.
+      expect(@item_1.best_date).to eq(@invoice_1.updated_at)
+    end
+  end
 end
--- spec/requests/item_request_spec.rb
+++ spec/requests/item_request_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'time'
 
 describe "Items API" do
   it "shows a list of items" do
@@ -84,5 +85,70 @@ describe "Items API" do
 
     expect(response).to be_successful
     expect(returned_item["data"]["id"]).to eq(item.id.to_s)
+  end
+end
+
+describe 'items stats' do
+  before :each do
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+    @merchant_3 = create(:merchant)
+
+    @customer_1 = create(:customer)
+    @customer_2 = create(:customer)
+    @customer_3 = create(:customer)
+
+    @item_1 = create(:item, merchant: @merchant_1)
+    @item_2 = create(:item, merchant: @merchant_2)
+    @item_3 = create(:item, merchant: @merchant_3)
+
+    @invoice_1 = create(:invoice, merchant: @merchant_1, customer: @customer_1, updated_at: '2012-03-27 14:54:09 UTC')
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice_1, quantity: 1, unit_price: 2000)
+
+    @invoice_2 = create(:invoice, merchant: @merchant_2, customer: @customer_1,  updated_at: '2012-03-24 14:54:09 UTC')
+    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice_2, quantity: 3, unit_price: 300)
+
+    @invoice_3 = create(:invoice, merchant: @merchant_3, customer: @customer_2,  updated_at: '2012-03-23 14:54:09 UTC')
+    @invoice_item_3 = create(:invoice_item, item: @item_3, invoice: @invoice_3, quantity: 5, unit_price: 200)
+
+    @invoice_4 = create(:invoice, merchant: @merchant_1, customer: @customer_2,  updated_at: '2012-03-23 14:54:09 UTC')
+    @invoice_item_4 = create(:invoice_item, item: @item_1, invoice: @invoice_4, quantity: 1, unit_price: 200)
+
+    @transaction_1 = create(:transaction, invoice: @invoice_1, result: 'success', updated_at: '2012-03-27 14:54:09 UTC')
+    @transaction_2 = create(:transaction, invoice: @invoice_2, result: 'success', updated_at: '2012-03-27 14:54:09 UTC')
+    @transaction_3 = create(:transaction, invoice: @invoice_3, result: 'success', updated_at: '2012-03-26 14:54:09 UTC')
+  end
+
+  it 'returns the top x items ranked by total revenue generated' do
+    count =  3
+    item_id = @item_1.id
+
+    get "/api/v1/items/most_revenue?quantity=#{count}"
+    returned_item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(returned_item["data"][0]["id"]).to eq(item_id.to_s)
+  end
+
+  it 'returns the top x items ranked by total number items sold' do
+    count =  3
+    item_id = @item_3.id
+
+    get "/api/v1/items/most_items?quantity=#{count}"
+    returned_item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(returned_item["data"][0]["id"]).to eq(item_id.to_s)
+  end
+
+  it 'returns the date with the most sales for the given item by invoice date' do
+    invoice_date = '2012-03-27 14:54:09 UTC'
+    item_id = @item_3.id
+
+    get "/api/v1/items/#{item_id}/best_day"
+    returned_item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(returned_item["data"]["best_day"][0..9]).to eq(invoice_date[0..9])
   end
 end
