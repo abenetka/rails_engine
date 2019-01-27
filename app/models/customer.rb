50aec5f4a023b02d885deb95bac3021f17ca239b
abenetka <ali.benetka@gmail.com>
2019-01-27 10:25:55 -0700

    Adds implementation of favorite merchant method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,15 @@
 class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
   has_many :invoices
+  has_many :merchants, through: :invoices
 
-  def self.successful_transactions
-    Customer.joins(invoices: :transactions)
-    .where(transactions: {result: 0})
+  def favorite_merchant
+    Merchant.joins(invoices: :transactions)
+      .select('merchants.*, count(transactions.id) as total_transactions')
+      .where(transactions: {result: 0}, invoices: {customer_id: self.id})
+      .group(:id)
+      .order('total_transactions desc')
+      .limit(1)
   end
+
 end
