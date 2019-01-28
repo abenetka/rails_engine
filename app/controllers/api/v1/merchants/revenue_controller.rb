17f0baccf6fdd00b95dc1b52403cebfda4422cb1
abenetka <ali.benetka@gmail.com>
2019-01-27 17:03:51 -0700

    Updates controller for spec harness

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def index
-    render json: { data: {revenue_by_date: {all_merchants: Merchant.total_revenue_by_date(params[:date])}}}
+    render json: { total_revenue: Merchant.total_revenue_by_date(params[:date])}
   end
 
   def show
     merchant = Merchant.find(params[:id])
     if params[:date]
-      data = { data: {revenue_for_invoice_date: {merchant: merchant.single_merchant_revenue_by_date(params[:date])}}}
+      data = { data: {revenue: merchant.single_merchant_revenue_by_date(params[:date])}}
     else
-      data = { data: {revenue_for_merchant: {merchant: merchant.total_revenue_for_merchant}}}
+      data = { data: {revenue: merchant.total_revenue_for_merchant}}
     end
     render json: data
   end
